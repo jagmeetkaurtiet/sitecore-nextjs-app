44fc5bb218bc11eabcb4a2768808d976c831c652
Jagmeet Kaur <Jagmeet.Kaur1@cbre.com>
2024-06-20 11:24:47 -0500

    netlify issue with src/temp

---
+++ src/temp/next-config-plugins.js
@@ -0,0 +1,5 @@
+exports.componentPropsPlugin = require('../lib/next-config/plugins/component-props');
+exports.corsHeaderPlugin = require('../lib/next-config/plugins/cors-header');
+exports.disconnectedPlugin = require('../lib/next-config/plugins/disconnected');
+exports.graphqlPlugin = require('../lib/next-config/plugins/graphql');
+exports.styleguidePlugin = require('../lib/next-config/plugins/styleguide');
